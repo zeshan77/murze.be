fee1aebc6c08b5f4cb3b505ba6c1012197d84294
Muhammed Zeshan <muhammad.zeshan@aboutyou.de>
2018-10-14 11:27:19 +0200

    updates seo

--- resources/views/front/layouts/_partials/seo.blade.php
+++ resources/views/front/layouts/_partials/seo.blade.php
@@ -1,28 +1,27 @@
-<meta name="description" content="Freek Van der Herten is a developer and partner at Spatie.">
-
-<meta property="og:site_name" content="murze.be">
+<meta name="description" content="Zeshan Khattak is a certified laravel developer living in Hamburg, Germany.">
+<meta property="og:site_name" content="zeshankhattak.com">
 <meta property="og:locale" content="en_US">
-<meta property="og:description" content="Freek Van der Herten is a Laravel enthousiast and PHP developer at Spatie.">
+<meta property="og:description" content="Zeshan Khattak is a certified laravel developer works in Hamburg, Germany as a senior PHP developer.">
 <meta property="og:url" content="{{ request()->fullUrl() }}">
-<meta property="og:image" content="https://murze.be/images/avatar-boxed.jpg">
+<meta property="og:image" content="https://zeshankhattak.com/images/zeshan.jpg">
 
 <script type='application/ld+json'>
     {
         "@context":"http:\/\/schema.org",
         "@type":"WebSite",
         "@id":"#website",
-        "url":"https:\/\/murze.be\/",
-        "name":"murze.be",
-        "alternateName":"A blog on modern PHP and Laravel"
+        "url":"https:\/\/zeshankhattak.com\/",
+        "name":"zeshankhattak.com",
+        "alternateName":"A certified laravel developer work as a senior PHP Developer."
     }
 </script>
 <script type='application/ld+json'>
     {
         "@context":"http:\/\/schema.org",
         "@type":"Person",
-        "sameAs":["https:\/\/twitter.com\/freekmurze"],
+        "sameAs":["https:\/\/twitter.com\/zeshan"],
         "@id":"#person",
-        "name":"Freek Van der Herten"
+        "name":"Zeshan Khattak"
     }
 </script>
 
