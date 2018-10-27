cc486a27505865fc62de0861c8ae13c94359e400
Muhammed Zeshan <muhammad.zeshan@aboutyou.de>
2018-10-27 14:48:04 +0200

    Google analytis added.

--- resources/views/front/layouts/_partials/analytics.blade.php
+++ resources/views/front/layouts/_partials/analytics.blade.php
@@ -1,17 +1,17 @@
 @if(app()->environment('production'))
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script nonce="{{ cspNonce() }}" async src="https://www.googletagmanager.com/gtag/js?id=UA-57290920-1"></script>
-    <script nonce="{{ cspNonce() }}" >
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58617688-4"></script>
+    <script>
         window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
+        function gtag(){
             dataLayer.push(arguments);
         }
 
         gtag('js', new Date());
 
-        gtag('config', 'UA-57290920-1');
+        gtag('config', 'UA-58617688-4');
     </script>
 
 @endif
